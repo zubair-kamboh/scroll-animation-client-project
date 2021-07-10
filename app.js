9897acfe47b0b2e747845a8af61d18faac12215d
Zubair Ali <zubairkamboh9010@gmail.com>
2021-07-10 15:43:11 +0500

    Character Change Event Added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// console.clear()
-
 gsap.defaults({ overwrite: 'auto' })
 
 gsap.set('.left__section > *', { xPercent: -50, yPercent: -50 })
@@ -9,6 +7,23 @@ const ST = ScrollTrigger.create({
   trigger: '.right__section',
   start: 'top top',
   end: 'bottom bottom',
-  //   onUpdate: getCurrentSection,
   pin: '.left__section',
+})
+
+// CHANGE SVG ON CLICK
+// Variables
+let svgPath = document.querySelector('.left__section svg path')
+const buttons = document.querySelectorAll('.right__section button')
+
+// Functions
+const onBtnClick = (e) => {
+  const targetBtn = e.target
+  let currentSvgPath = targetBtn.childNodes[1].childNodes[1].getAttribute('d')
+
+  svgPath.setAttribute('d', currentSvgPath)
+}
+
+buttons.forEach((button, i) => {
+  // On Button Click
+  button.addEventListener('click', onBtnClick)
 })
